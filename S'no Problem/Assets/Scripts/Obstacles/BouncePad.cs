40403bb6473f1a8e09ee404c649f2ebe85e1f4e5
ZclipseCode <bmichaelbusiness@gmail.com>
2023-10-15 01:22:16 -0500

    bounce pad

---
+++ S'no Problem/Assets/Scripts/Obstacles/BouncePad.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    [SerializeField] float bounceForce = 1;
+    float horizontalBounceForce;
+
+    private void Start()
+    {
+        horizontalBounceForce = bounceForce / 4;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Snowball"))
+        {
+            Bounce(collision.GetComponent<Rigidbody2D>());
+        }
+    }
+
+    void Bounce(Rigidbody2D rb)
+    {
+        float horizontalDirection = rb.velocity.x / Mathf.Abs(rb.velocity.x);
+
+        rb.AddForce(new Vector2(horizontalBounceForce * horizontalDirection, bounceForce));
+    }
+}
